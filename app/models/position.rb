8485f668fd4c3f6c37fc3012b5633307ef8a18cb
Nikita Nyatin <yesnik@yandex.ru>
2015-01-29 14:54:33 +0500

    Add seeds.rb

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,5 +1,6 @@
 class Position < ActiveRecord::Base
   DIRECTOR_ID = 2.freeze
+  SPECIALIST_ID = 1.freeze
   
   has_many :employees
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Создадим должности для пользователей
+Position.create([
+  {title: 'специалист'},
+  {title: 'директор'},
+  {title: 'бухгалтер'},
+  {title: 'электрик'}
+])
+
+specialist_id = Position::SPECIALIST_ID
+Employee.create([
+  {first_name: 'Иван', middle_name: 'Владимирович', 
+    last_name: 'Дорин', position_id: specialist_id},
+  {first_name: 'Ксения', middle_name: 'Ивановна', 
+    last_name: 'Рубцова', position_id: specialist_id},
+  {first_name: 'Даниил', middle_name: 'Александрович', 
+    last_name: 'Рыбаков', position_id: specialist_id},
+  {first_name: 'Петр', middle_name: 'Алексеевич', 
+    last_name: 'Донцов', position_id: specialist_id},
+  {first_name: 'Мария', middle_name: 'Алексеевна', 
+    last_name: 'Петрова', position_id: specialist_id},
+])
+
+Company.create([
+  {title: 'ОАО Газпром'},
+  {title: 'ЗАО Тандер'},
+  {title: 'ОАО СКБ-банк'},
+  {title: 'ОАО СКБ-Контур'},
+  {title: 'ОАО Яндекс'},
+  {title: 'ООО Пикник'},
+])
+
+c = Company.first
+c.employees << Employee.find(1)
+c.employees << Employee.find(2)
+c.employees << Employee.find(3)
+c.save
+
+c2 = Company.second
+c2.employees << Employee.find(3)
+c2.employees << Employee.find(4)
+c2.employees << Employee.find(5)
+c2.save
+
+c3 = Company.second
+c3.employees << Employee.find(2)
+c3.employees << Employee.find(3)
+c3.employees << Employee.find(5)
+c3.save
